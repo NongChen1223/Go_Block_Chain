10b0e85f79c49cfa71848ebbdd843c2315a8ebe9
NongChen <2584533077@qq.com>
2024-12-17 23:55:58 +0800

    feat: 新增中间件目录

--- system/controllers/sys_login_control.go
+++ system/controllers/sys_login_control.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_server/system/dto"
+)
+
+func Login(ctx *gin.Context) {
+	var logReq *dto.LoginReq
+
+	if err := ctx.ShouldBindJSON(&logReq); err != nil {
+
+	}
+}
--- system/dto/sys_login_dto.go
+++ system/dto/sys_login_dto.go
@@ -1,9 +1,14 @@
 package dto
 
-type UserLoginReq struct {
+type LoginReq struct {
 	Username  string `json:"username" binding:"required"`  // 用户名，必填
 	Password  string `json:"password" binding:"required"`  // 密码，必填
 	AutoLogin bool   `json:"autoLogin"`                    // 是否自动登录，可选
 	GrantType string `json:"grantType" binding:"required"` // 授权类型，必填
 	Type      string `json:"type" binding:"required"`      // 登录类型，必填
+}
+
+type LoginRes struct {
+	Code        int    `json:"code"`
+	AccessToken string `json:"access_token"` // 访问令牌
 }
